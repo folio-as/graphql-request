5bbe57762b0b0a9e044a0964ef9508c346b511e2
Simen Bekkhus <sbekkhus91@gmail.com>
2023-05-30 16:08:23 +0200

    use non-node formdata
    https://github.com/jasonkuhrt/graphql-request/issues/384

--- src/createRequestBody.ts
+++ src/createRequestBody.ts
@@ -46,9 +46,7 @@ export default function createRequestBody(
     return jsonSerializer.stringify(payload)
   }
 
-  const Form = typeof FormData === 'undefined' ? FormDataNode : FormData
-
-  const form = new Form()
+  const form = new FormDataNode()
 
   form.append('operations', jsonSerializer.stringify(clone))
 
